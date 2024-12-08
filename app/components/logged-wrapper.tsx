16fac8e066ac2b2b7c5272f749df53283829f845
Idoz <idozamir87@gmail.com>
2024-12-08 14:47:22 +0200

    typo

--- app/components/logged-wrapper.tsx
+++ app/components/logged-wrapper.tsx
@@ -29,7 +29,7 @@ export default function LoggedWrapper({ children }: { children: ReactNode }) {
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [navigate]);
 
   return (
     <AuthProvider>
